3154651d8313dcd6f57c7738521142e934f6fcfc
LAP-EHV-JV\Joost <joost.verberk@gmail.com>
2018-11-25 20:17:12 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(name='pushover',
+      version='0.1',
+      description='Simple Python wrapper for the Pushover API',
+      url='https://github.com/KoenradusXLVIII/Pushover',
+      author='Joost Verberk',
+      packages=setuptools.find_packages())
